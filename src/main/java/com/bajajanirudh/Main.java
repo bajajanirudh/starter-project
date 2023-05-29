14372b8352424a947a64d37b2e6d7c48723b502b
bajajanirudh <bajajanirudh@google.com>
2023-05-29 06:05:50 +0000

    Initial Commit

---
+++ src/main/java/com/bajajanirudh/Main.java
@@ -0,0 +1,19 @@
+package com.bajajanirudh;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+        int port = 9000;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        System.out.println("server started at " + port);
+        server.createContext("/", new RootHandler(port));
+        server.setExecutor(null);
+        server.start();
+    }
+}
